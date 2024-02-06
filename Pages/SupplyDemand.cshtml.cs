eafee25324632f53fb7dfb36cb0bea68f197f6e1
jbd0017 <jbd0017@mix.wvu.edu>
2024-02-06 13:04:02 -0500

    Add project files.

---
+++ Pages/SupplyDemand.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DriverSite.Pages
+{
+    public class SupplyDemandModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
